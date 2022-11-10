876aa5727ec2756ebb789cd4467a586a983de0ec
Michael Liu <michael.liu95@gmail.com>
2022-11-10 18:10:19 -0500

    WIP contract updates

--- packages/smart-contracts/scripts/test-deploy-invoice-nft.ts
+++ packages/smart-contracts/scripts/test-deploy-invoice-nft.ts
@@ -3,16 +3,12 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 export async function deployInvoiceNFT(hre: HardhatRuntimeEnvironment) {
   try {
     const [deployer] = await hre.ethers.getSigners();
-
     const InvoiceNFT = await hre.ethers.getContractFactory('InvoiceNFT', deployer);
     const invoiceNFT = await InvoiceNFT.deploy('Invoice NFT', 'INFT');
     await invoiceNFT.deployed();
-
     console.log('InvoiceNFT Contract deployed: ' + invoiceNFT.address);
-
     const mintRole = await invoiceNFT.MINTER_ROLE();
     await invoiceNFT.grantRole(mintRole, deployer.address);
-
     console.log(`deployer ${deployer.address} has mint role of InvoiceNFT`);
   } catch (e) {
     console.error(e);

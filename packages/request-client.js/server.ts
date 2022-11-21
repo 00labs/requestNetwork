3bd6ee030a93a19aa38ed29b6d5878572509de31
bin <bin@57blocks.com>
2022-11-21 16:05:21 +0800

    1. simply server code 2. increase contracts block number

--- packages/request-client.js/server.ts
+++ packages/request-client.js/server.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { EthereumPrivateKeySignatureProvider } from '@requestnetwork/epk-signature';
+// import { EthereumPrivateKeySignatureProvider } from '@requestnetwork/epk-signature';
 import * as RequestNetwork from '.';
 
 const app = express();
@@ -11,33 +11,33 @@ app.listen(port, () => {
 
 app.get('/request', async function (req, res) {
   // this is a local ERC20 token deployed on ganache
-  const localToken = '0xf17FF940864351631b1be3ac03702dEA085ba51c';
-  const PAYEE_PRIVATE_KEY = '0xa8e85c1c3bdcb3221493d1f43dfd9e7dd966c799fd46bd0d6a45a97f9df59adb';
+  // const localToken = '0xf17FF940864351631b1be3ac03702dEA085ba51c';
+  // const PAYEE_PRIVATE_KEY = '0xa8e85c1c3bdcb3221493d1f43dfd9e7dd966c799fd46bd0d6a45a97f9df59adb';
 
   //#region Identity setup
   // Payee Identity & Private key
-  const payeeSignatureInfo = {
-    method: RequestNetwork.Types.Signature.METHOD.ECDSA,
-    privateKey: PAYEE_PRIVATE_KEY,
-  };
+  // const payeeSignatureInfo = {
+  //   method: RequestNetwork.Types.Signature.METHOD.ECDSA,
+  //   privateKey: PAYEE_PRIVATE_KEY,
+  // };
   //#endregion
 
   //#region RequestNetwork setup
   // ✏️ Signature provider
-  const signatureProvider: RequestNetwork.Types.SignatureProvider.ISignatureProvider =
-    new EthereumPrivateKeySignatureProvider(payeeSignatureInfo);
+  // const signatureProvider: RequestNetwork.Types.SignatureProvider.ISignatureProvider =
+  //   new EthereumPrivateKeySignatureProvider(payeeSignatureInfo);
 
   const requestNetwork = new RequestNetwork.RequestNetwork({
-    signatureProvider,
-    currencies: [
-      {
-        address: localToken,
-        decimals: 18,
-        network: 'goerli',
-        symbol: 'ERC20',
-        type: RequestNetwork.Types.RequestLogic.CURRENCY.ERC20,
-      },
-    ],
+    // signatureProvider,
+    // currencies: [
+    //   {
+    //     address: localToken,
+    //     decimals: 18,
+    //     network: 'goerli',
+    //     symbol: 'ERC20',
+    //     type: RequestNetwork.Types.RequestLogic.CURRENCY.ERC20,
+    //   },
+    // ],
   });
 
   const requestID: string = req.query.id as string;

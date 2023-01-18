a583c3cd147a7ea88afc47d85387de25c7f60386
Michael Liu <michael.liu95@gmail.com>
2023-01-18 13:55:10 -0500

    add new files

---
+++ packages/smart-contracts/src/lib/artifacts/ERC20TransferrableReceivable/index.ts
@@ -0,0 +1,29 @@
+import { ContractArtifact } from '../../ContractArtifact';
+
+import { abi as ABI_0_1_0 } from './0.1.0.json';
+// @ts-ignore Cannot find module
+import type { ERC20TransferrableReceivable } from '../../../types/ERC20TransferrableReceivable';
+
+export const erc20TransferrableReceivableArtifact =
+  new ContractArtifact<ERC20TransferrableReceivable>(
+    {
+      '0.1.0': {
+        abi: ABI_0_1_0,
+        deployment: {
+          private: {
+            address: '0x2e335F247E91caa168c64b63104C4475b2af3942',
+            creationBlockNumber: 0,
+          },
+          goerli: {
+            address: '0x9aEBB4B8abf7afC96dC00f707F766499C5EbeDF1',
+            creationBlockNumber: 7956654,
+          },
+          matic: {
+            address: '0xf98b8a94edbc9628b7b2141465980f2c3acab23f',
+            creationBlockNumber: 36487297,
+          },
+        },
+      },
+    },
+    '0.1.0',
+  );

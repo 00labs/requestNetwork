9643cc2555165c98e7420beb684604181782c683
Michael Liu <michael.liu95@gmail.com>
2023-01-18 13:55:10 -0500

    add new files

---
+++ packages/smart-contracts/src/contracts/ERC20TransferrableReceivable.sol
@@ -0,0 +1,117 @@
+// Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract ERC20TransferrableReceivable is ERC721URIStorage {
+    using SafeERC20 for IERC20;
+    using Counters for Counters.Counter;
+    Counters.Counter private _paymentId;
+
+    // ERC20 that one ERC20TransferrableReceivable is paid in
+    mapping(uint256 => address) public assetToken;
+
+    mapping(address => uint256[]) private _tokenIds;
+    mapping(uint256 => uint256) private _tokenIdIndexes;
+
+    event Payment(
+        address sender,
+        address recipient,
+        uint256 tokenId,
+        address assetAddress,
+        uint256 amount,
+        uint256 paymentId,
+        bytes indexed paymentReference
+    );
+
+    error zeroAddressProvided(); // 0x5ff75ab0
+    error tokenIdAlreadyExists();
+    error zeroAmountProvided(); // 0x71799f2a
+    error noAssetToken();
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function payOwner(
+        uint256 tokenId,
+        uint256 amount,
+        bytes calldata paymentReference
+    ) external {
+        if (amount == 0) revert zeroAmountProvided();
+        address owner = ownerOf(tokenId);
+        _paymentId.increment();
+        address assetAddress = assetToken[tokenId];
+        IERC20(assetToken[tokenId]).safeTransferFrom(msg.sender, owner, amount);
+
+        emit Payment(
+            msg.sender,
+            owner,
+            tokenId,
+            assetAddress,
+            amount,
+            _paymentId.current(),
+            paymentReference
+        );
+    }
+
+    function mint(
+        address recipient,
+        uint256 tokenId,
+        address erc20Addr,
+        string memory tokenURI
+    ) external {
+        if (recipient == address(0) || erc20Addr == address(0)) revert zeroAddressProvided();
+        if (_exists(tokenId)) revert tokenIdAlreadyExists();
+
+        _mint(recipient, tokenId);
+        assetToken[tokenId] = erc20Addr;
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    function getIds(address account) external view returns (uint256[] memory) {
+        return _tokenIds[account];
+    }
+
+    function getTokenIdIndex(uint256 tokenId) external view returns (uint256) {
+        return _tokenIdIndexes[tokenId];
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        if (from != address(0)) {
+            uint256 index = _tokenIdIndexes[tokenId];
+            if (index > 0) {
+                index = index - 1;
+                uint256[] storage tokenIds = _tokenIds[from];
+                uint256 len = tokenIds.length;
+                if (index < len && tokenIds[index] > 0) {
+                    if (index < len - 1) {
+                        tokenIds[index] = tokenIds[len - 1];
+                        _tokenIdIndexes[tokenIds[len - 1]] = index + 1;
+                    }
+                    tokenIds.pop();
+                }
+            }
+        }
+        if (to != address(0)) {
+            uint256[] storage tokenIds = _tokenIds[to];
+            tokenIds.push(tokenId);
+            _tokenIdIndexes[tokenId] = tokenIds.length;
+        }
+    }
+}
---
+++ packages/smart-contracts/test/contracts/ERC20TransferrableReceivable.test.ts
@@ -0,0 +1,155 @@
+import '@nomiclabs/hardhat-ethers';
+import { Signer } from 'ethers';
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import {
+  TestERC20__factory,
+  TestERC20,
+  ERC20TransferrableReceivable__factory,
+  ERC20TransferrableReceivable,
+} from '../../src/types';
+import { BigNumber as BN } from 'ethers';
+
+const BASE_DECIMAL = BN.from(10).pow(BN.from(18));
+
+describe.only('contract: ERC20TransferrableReceivable', () => {
+  let deployer: Signer;
+  let user1: Signer, user1Addr: string;
+  let user2: Signer, user2Addr: string;
+  let user3: Signer, user3Addr: string;
+
+  let testToken: TestERC20, receivable: ERC20TransferrableReceivable;
+
+  before(async function () {
+    [deployer, user1, user2, user3] = await ethers.getSigners();
+    user1Addr = await user1.getAddress();
+    user2Addr = await user2.getAddress();
+    user3Addr = await user3.getAddress();
+  });
+
+  beforeEach(async function () {
+    testToken = await new TestERC20__factory(deployer).deploy(BN.from(1000000).mul(BASE_DECIMAL));
+    receivable = await new ERC20TransferrableReceivable__factory(deployer).deploy(
+      'Request Transferrable Receivable',
+      'RReceivable',
+    );
+
+    await testToken.approve(receivable.address, ethers.constants.MaxUint256);
+  });
+
+  async function verifyReceivables(userAddr: string, receivableIds: any) {
+    const ids = await receivable.getIds(userAddr);
+    expect(ids.toString()).equals(receivableIds.toString());
+  }
+
+  async function verifyReceivableIndexes(indexes: any) {
+    const res = [];
+    for (let i = 1; i <= 5; i++) {
+      res[i - 1] = await receivable.getTokenIdIndex(i);
+    }
+    expect(res.toString()).equals(indexes.toString());
+  }
+
+  describe('mint', async function () {
+    it('revert with empty recipient', async function () {
+      await expect(receivable.mint(ethers.constants.AddressZero, 1, testToken.address, '0x01')).to
+        .be.reverted;
+      // 'zeroAddressProvided()'
+    });
+
+    it('revert with asset address', async function () {
+      await expect(receivable.mint(user1Addr, 1, ethers.constants.AddressZero, '0x01')).to.be
+        .reverted;
+      // 'zeroAddressProvided()'
+    });
+
+    it('revert with duplicated tokenId', async function () {
+      await receivable.mint(user1Addr, 1, testToken.address, '0x01');
+      await expect(receivable.mint(user1Addr, 1, testToken.address, '0x01')).reverted;
+      // 'tokenIdAlreadyExists()'
+    });
+
+    it('success', async function () {
+      const tokenId = BN.from('0x34cc5f0224acb0544a9d325f8f2160c53130ba4671849472f2a96a35c93a78d6');
+
+      const metadata = ethers.utils.base64.encode('0x01');
+      console.log('metadata: ' + metadata);
+      await receivable.mint(user1Addr, tokenId, testToken.address, metadata);
+      const owner = await receivable.ownerOf(tokenId);
+      const md = await receivable.tokenURI(tokenId);
+      expect(owner).equals(user1Addr);
+      expect(md).equals(metadata);
+    });
+
+    it('list receivables', async function () {
+      const metadata = ethers.utils.base64.encode('0x01');
+      await receivable.mint(user1Addr, 1, testToken.address, metadata);
+      await receivable.mint(user1Addr, 2, testToken.address, metadata);
+      await receivable.mint(user1Addr, 3, testToken.address, metadata);
+      await verifyReceivables(user1Addr, [1, 2, 3]);
+      await receivable.mint(user2Addr, 4, testToken.address, metadata);
+      await receivable.mint(user2Addr, 5, testToken.address, metadata);
+      await verifyReceivables(user2Addr, [4, 5]);
+      await receivable.connect(user1).transferFrom(user1Addr, user2Addr, 1);
+      await verifyReceivables(user1Addr, [3, 2]);
+      await verifyReceivables(user2Addr, [4, 5, 1]);
+      await verifyReceivableIndexes([3, 2, 1, 1, 2]);
+      await receivable
+        .connect(user2)
+        ['safeTransferFrom(address,address,uint256)'](user2Addr, user1Addr, 5);
+      await verifyReceivables(user1Addr, [3, 2, 5]);
+      await verifyReceivables(user2Addr, [4, 1]);
+      await verifyReceivableIndexes([2, 2, 1, 1, 3]);
+      await receivable.connect(user1).approve(user3Addr, 3);
+      await receivable.connect(user1).approve(user3Addr, 2);
+      await receivable.connect(user1).approve(user3Addr, 5);
+      await receivable.connect(user2).approve(user3Addr, 4);
+      await verifyReceivableIndexes([2, 2, 1, 1, 3]);
+      await receivable.connect(user3).transferFrom(user1Addr, user3Addr, 5);
+      await receivable.connect(user3).transferFrom(user1Addr, user3Addr, 2);
+      await receivable.connect(user3).transferFrom(user1Addr, user3Addr, 3);
+      await receivable.connect(user3).transferFrom(user2Addr, user3Addr, 4);
+      await verifyReceivables(user1Addr, []);
+      await verifyReceivables(user2Addr, [1]);
+      await verifyReceivables(user3Addr, [5, 2, 3, 4]);
+      await verifyReceivableIndexes([1, 2, 3, 4, 1]);
+    });
+  });
+
+  describe('payOwner', async function () {
+    it('revert with zero amount', async function () {
+      const tokenId = 1;
+      const metadata = ethers.utils.base64.encode('0x01');
+      await receivable.mint(await user1.getAddress(), tokenId, testToken.address, metadata);
+      await expect(receivable.payOwner(tokenId, 0, '0x01')).reverted;
+      // 'zeroAmountProvided()'
+    });
+
+    it('success for original owner', async function () {
+      const tokenId = 1;
+      const metadata = ethers.utils.base64.encode('0x01');
+      const user1Addr = await user1.getAddress();
+      await receivable.mint(user1Addr, tokenId, testToken.address, metadata);
+      const amount = BN.from(100).mul(BASE_DECIMAL);
+      const beforeBal = await testToken.balanceOf(user1Addr);
+      await receivable.payOwner(tokenId, amount, '0x02');
+      const afterBal = await testToken.balanceOf(user1Addr);
+      expect(amount).equals(afterBal.sub(beforeBal));
+    });
+
+    it('success for new owner', async function () {
+      const tokenId = 1;
+      const metadata = ethers.utils.base64.encode('0x01');
+      const user1Addr = await user1.getAddress();
+      const user2Addr = await user2.getAddress();
+      await receivable.mint(user1Addr, tokenId, testToken.address, metadata);
+      await receivable.connect(user1).transferFrom(user1Addr, user2Addr, tokenId);
+
+      const amount = BN.from(100).mul(BASE_DECIMAL);
+      const beforeBal = await testToken.balanceOf(user2Addr);
+      await receivable.payOwner(tokenId, amount, '0x01');
+      const afterBal = await testToken.balanceOf(user2Addr);
+      expect(amount).equals(afterBal.sub(beforeBal));
+    });
+  });
+});

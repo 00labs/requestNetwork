6446c64d6e5a34c6e006c78f7ff743cea99f2dbb
Michael Liu <michael.liu95@gmail.com>
2023-01-20 16:49:00 -0500

    Update contract variable names, use proxy contract call

--- packages/smart-contracts/scripts/test-deploy-erc20-transferrable-receivable.ts
+++ packages/smart-contracts/scripts/test-deploy-erc20-transferrable-receivable.ts
@@ -1,24 +1,26 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { deployOne } from './deploy-one';
 
-export async function deployERC20TransferrableReceivable(hre: HardhatRuntimeEnvironment) {
-  try {
-    const [deployer] = await hre.ethers.getSigners();
-    const ERC20TransferrableReceivable = await hre.ethers.getContractFactory(
-      'ERC20TransferrableReceivable',
-      deployer,
-    );
-    const erc20TransferrableReceivable = await ERC20TransferrableReceivable.deploy(
-      'Request Transferrable Receivable',
-      'RReceivable',
-      {
-        gasPrice: 40000000000,
-      },
-    );
-    await erc20TransferrableReceivable.deployed();
-    console.log(
-      'ERC20TransferrableReceivable Contract deployed: ' + erc20TransferrableReceivable.address,
-    );
-  } catch (e) {
-    console.error(e);
-  }
+export async function deployERC20TransferrableReceivable(
+  args: any,
+  hre: HardhatRuntimeEnvironment,
+  mainPaymentAddresses: any,
+) {
+  // Deploy ERC20 Transferrable Receivable
+  const { address: ERC20TransferrableReceivableAddress } = await deployOne(
+    args,
+    hre,
+    'ERC20TransferrableReceivable',
+    {
+      constructorArguments: [
+        'Request Transferrable Receivable',
+        'RReceivable',
+        mainPaymentAddresses.ERC20FeeProxyAddress,
+      ],
+    },
+  );
+
+  console.log(
+    `ERC20TransferrableReceivable Contract deployed: ${ERC20TransferrableReceivableAddress}`,
+  );
 }

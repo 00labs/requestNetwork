fe31d407acff6a4f590dff217ce28945fd5cee0c
bin <bin@57blocks.com>
2023-02-09 10:44:30 +0800

    display errors

--- packages/ethereum-storage/src/smart-contract-manager.ts
+++ packages/ethereum-storage/src/smart-contract-manager.ts
@@ -381,7 +381,7 @@ export default class SmartContractManager {
               nonce,
             });
             this.logger.error(`Failed transaction: ${logObject}`);
-            reject(Error(`Ethereum transaction error:  ${transactionError}`));
+            reject(Error(`Ethereum transaction error:  ${JSON.stringify(transactionError)}`));
           }
         })
         .on('confirmation', (confirmationNumber: number, receiptAfterConfirmation: any) => {
@@ -534,7 +534,7 @@ export default class SmartContractManager {
               nonce,
             });
             this.logger.error(`Failed transaction: ${logObject}`);
-            reject(Error(`Ethereum transaction error:  ${transactionError}`));
+            reject(Error(`Ethereum transaction error:  ${JSON.stringify(transactionError)}`));
           }
         })
         .on('confirmation', (confirmationNumber: number, receiptAfterConfirmation: any) => {
